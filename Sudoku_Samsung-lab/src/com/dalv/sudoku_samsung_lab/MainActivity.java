e863d15eb7164d1768e0b1b902532796ad79fec1
dalv666 <dalv6666@gmail.com>
2014-02-11 22:08:36 +0400

    Add a lot...

--- Sudoku_Samsung-lab/src/com/dalv/sudoku_samsung_lab/MainActivity.java
+++ Sudoku_Samsung-lab/src/com/dalv/sudoku_samsung_lab/MainActivity.java
@@ -1,22 +1,30 @@
 package com.dalv.sudoku_samsung_lab;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SudokuFragment firstFragment = new SudokuFragment(); 
 		setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+        .add(R.id.FLgame, firstFragment).commit();
 	}
+	
+	
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d("dalvv", data.getStringExtra("value")+" Activity");
+	    if (data == null || data.getStringExtra("value").equals(getResources().getString(R.string.back))) {return;}
+	    String name = data.getStringExtra("value");
+	    SudokuFragment frag = (SudokuFragment) (getSupportFragmentManager().findFragmentById(R.id.FLgame));
+	    frag.changeValueCell(name);
+		super.onActivityResult(requestCode, resultCode, data);
 	}
-
 }
